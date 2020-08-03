6e9aaddfbe4636c15811da3144dad60fbb64d1ad
Kyle Wright <kylejamwright@gmail.com>
2020-08-03 16:51:10 -0600

    Implemented Create in the service layer

--- routes/api/trails.js
+++ routes/api/trails.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const {
-    readTrails
+    readTrails,
+    createTrail
 } = require('../../data/trails');
 
 /* GET trails listing. */
@@ -9,5 +10,11 @@ router.get('/', async function(req, res, next) {
     const data = await readTrails();
     res.send(data);
 });
+
+router.post('/', async function(req, res, next) {
+    const body = req.body;
+    const data = await createTrail(body);
+    res.send(data);
+})
 
 module.exports = router;
